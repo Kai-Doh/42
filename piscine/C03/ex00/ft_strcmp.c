2d105bb5b9583a3a465e9c4e3f04486a90ff3633
Kai Doh <karl.tiomico@gmail.com>
2024-08-28 22:47:11 +0200

    added main functions

--- piscine/C03/ex00/ft_strcmp.c
+++ piscine/C03/ex00/ft_strcmp.c
@@ -1,11 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktiomico <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 10:56:44 by ktiomico          #+#    #+#             */
+/*   Updated: 2024/08/23 11:55:58 by ktiomico         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*
+#include <stdio.h>
+*/
 int	ft_strcmp(char *s1, char *s2)
 {
-	int	i;
+	int	index;
 
-	i = 0;
-	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+	index = 0;
+	while (s1[index] == s2[index] && (s1[index] || s2[index]))
 	{
-		i++;
+		index++;
 	}
-	return (s1[i] - s2[i]);
+	return (s1[index] - s2[index]);
 }
+/*
+int	main(void)
+{
+	char	*s1 = "Hello World";
+	char 	*s2 = "Hello world";
+
+	ft_strcmp(s1, s2);
+	printf("%d",ft_strcmp(s1, s2));
+}
+*/
