fb7df895c5aa57776b633fa0460b8e10e0a246c6
Kai Doh <karl.tiomico@gmail.com>
2024-09-01 23:26:17 +0200

    main ex04 commented

--- piscine/C05/ex04/ft_fibonacci.c
+++ piscine/C05/ex04/ft_fibonacci.c
@@ -6,12 +6,12 @@
 /*   By: ktiomico <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 23:11:27 by ktiomico          #+#    #+#             */
-/*   Updated: 2024/09/01 23:17:49 by ktiomico         ###   ########.fr       */
+/*   Updated: 2024/09/01 23:25:46 by ktiomico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+/*
 #include <stdio.h>
-
+*/
 int	ft_fibonacci(int index)
 {
 	if (index < 0)
@@ -22,9 +22,10 @@ int	ft_fibonacci(int index)
 		return (0);
 	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
-
+/*
 int	main(void)
 {
 	printf("%d\n", ft_fibonacci(5));
 	return (0);
 }
+*/
