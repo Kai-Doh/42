f87bdb2d39d12901147ae2d04618c013281a6b99
Kai Doh <karl.tiomico@gmail.com>
2024-09-02 08:27:40 +0200

    ex03 complete

--- piscine/C06/ex03/ft_sort_params.c
+++ piscine/C06/ex03/ft_sort_params.c
@@ -6,14 +6,67 @@
 /*   By: ktiomico <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 11:50:33 by ktiomico          #+#    #+#             */
-/*   Updated: 2024/08/30 11:51:22 by ktiomico         ###   ########.fr       */
+/*   Updated: 2024/09/02 08:27:06 by ktiomico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+void ft_putchar(char c);
+
+char	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+void	ft_sort_params(int argc, char **argv)
+{
+	int i;
+	int j;
+	char *temp;
+	i = 1;
+	while (i < argc)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+			{
+				temp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		while (*argv[i])
+		{
+			ft_putchar(*argv[i]);
+			argv[i]++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+}
 
 void	ft_putchar(char c)
 {
 	write (1 , &c, 1);
 }
 
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+		ft_sort_params(argc, argv);
+}
