f1d6c826a8a2babac3dfb8954e4813e0c012038e
Kai Doh <karl.tiomico@gmail.com>
2024-09-04 22:43:00 +0200

    C08 and C07 corrections

--- piscine/C07/ex03/ft_strjoin.c
+++ piscine/C07/ex03/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ktiomico <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 11:29:28 by ktiomico          #+#    #+#             */
-/*   Updated: 2024/09/04 18:27:20 by ktiomico         ###   ########.fr       */
+/*   Updated: 2024/09/04 22:43:00 by ktiomico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,21 @@ int	ft_strlen(char *str)
 	return (length);
 }
 
-int	ft_malloc(int size, char **strs, char *sep)
+int	ft_count(int size, char **strs, char *sep)
 {
 	int	count;
 	int	j;
 
 	count = 0;
 	j = 0;
-	while (j++ < size)
+	while (j < size)
 	{
 		count = count + ft_strlen(strs[j]);
+		j++;
 		if (j < size)
-		count = count + ft_strlen(sep);
+		{
+			count = count + ft_strlen(sep);
+		}
 	}
 	return (count);
 }
@@ -48,19 +51,19 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	int		count;
 	char	*dest;
 
-	count = ft_malloc(size, strs, sep);
+	count = ft_count(size, strs, sep);
 	dest = (char *)malloc(sizeof(char) * count + 1);
 	if (dest == NULL)
 		return (NULL);
-	j = 1;
+	j = 0;
 	k = 0;
-	while (j < size + 1)
+	while (j < size)
 	{
 		i = 0;
 		while (strs[j][i])
 			dest[k++] = strs[j][i++];
 		i = 0;
-		while (sep[i] && j < size)
+		while (sep[i] && j < size - 1)
 			dest[k++] = sep[i++];
 		j++;
 	}
@@ -76,7 +79,7 @@ int	main(int argc, char **argv)
 
 	sep = " ";
 	if (argc != 1)
-		printf("%s", ft_strjoin(1, argv, sep));
-	free(ft_strjoin(argc, argv, sep));
+		printf("%s", ft_strjoin(3, argv, sep));
+	free(ft_strjoin(3, argv, sep));
 }
 */
--- piscine/C08/ex01/ft_boolean.h
+++ piscine/C08/ex01/ft_boolean.h
@@ -1,36 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ktiomico <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/03 00:21:58 by ktiomico          #+#    #+#             */
-/*   Updated: 2024/09/04 22:02:01 by ktiomico         ###   ########.fr       */
+/*   Created: 2024/09/04 22:12:45 by ktiomico          #+#    #+#             */
+/*   Updated: 2024/09/04 22:12:47 by ktiomico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-
-int	ft_ultimate_range(int **range, int min, int max)
-{
-	int	i;
-
-	if (range == NULL || min >= max)
-	{
-		if (range != NULL)
-			*range = NULL;
-		return (0);
-	}
-	*range = (int *)malloc(sizeof(int) * (max - min));
-	if (*range == NULL)
-		return (-1);
-	i = 0;
-	while (min < max)
-	{
-		(*range)[i] = min;
-		min++;
-		i++;
-	}
-	return (i);
-}
