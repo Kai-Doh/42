07f66ed45d0c4aa4571fb8f226227d1661a569a8
Kai-Doh <karl.tiomico@gmail.com>
2024-09-06 13:31:33 +0200

    ex00 completed

--- piscine/C08/ex00/ft.h
+++ piscine/C08/ex00/ft.h
@@ -6,7 +6,19 @@
 /*   By: ktiomico <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 22:12:33 by ktiomico          #+#    #+#             */
-/*   Updated: 2024/09/04 22:12:38 by ktiomico         ###   ########.fr       */
+/*   Updated: 2024/09/06 11:46:08 by ktiomico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_H
+# define FT_H
+
+# include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
--- piscine/C08/ex01/ft_boolean.h
+++ piscine/C08/ex01/ft_boolean.h
@@ -6,7 +6,16 @@
 /*   By: ktiomico <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 22:12:45 by ktiomico          #+#    #+#             */
-/*   Updated: 2024/09/04 22:12:47 by ktiomico         ###   ########.fr       */
+/*   Updated: 2024/09/06 11:52:05 by ktiomico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+void	ft_putstr(char *str);
+t_Bool	ft_is_even(int nbr);
+
+#endif
