04a48fa36df50c73223fa0819d5f187bc82662d2
Kai Doh <karl.tiomico@gmail.com>
2024-09-07 21:14:32 +0200

    update

--- piscine/C08/ex03/ft_point.h
+++ piscine/C08/ex03/ft_point.h
@@ -6,16 +6,17 @@
 /*   By: ktiomico <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 20:56:02 by ktiomico          #+#    #+#             */
-/*   Updated: 2024/09/07 21:08:29 by ktiomico         ###   ########.fr       */
+/*   Updated: 2024/09/07 21:14:24 by ktiomico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_POINT_H
 # define FT_POINT_H
 
-typedef struct strc_point
+typedef struct t_point
 {
 	int	x;
 	int	y;
 }	t_point;
+
 #endif
