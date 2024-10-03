93b013abba804ca6743f2b94461961d05730d98e
Kai Doh <karl.tiomico@gmail.com>
2024-10-03 21:51:18 +0200

    yaaay

--- Cursus/libft/ft_lstmap.c
+++ Cursus/libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ktiomico <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 17:24:47 by ktiomico          #+#    #+#             */
-/*   Updated: 2024/10/03 21:18:47 by ktiomico         ###   ########.fr       */
+/*   Updated: 2024/10/03 21:45:06 by ktiomico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,23 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
-	t_list	*new_elem;
-	void	*mapped_content;
+	t_list	*tmp;
+	void	*content;
 
 	new_list = NULL;
 	if (!lst || !f)
 		return (NULL);
 	while (lst)
 	{
-		mapped_content = f(lst->content);
-		new_elem = ft_lstnew(mapped_content);
-		if (!new_elem)
+		content = f(lst->content);
+		tmp = ft_lstnew(content);
+		if (!tmp)
 		{
-			del(mapped_content);
+			del(content);
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&new_list, new_elem);
+		ft_lstadd_back(&new_list, tmp);
 		lst = lst->next;
 	}
 	return (new_list);
